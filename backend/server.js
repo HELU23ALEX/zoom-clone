410d92c78746f99866c1835b09adfd09c418c793
HELU23ALEX <HELINAAALEMU@gmail.com>
2025-02-14 10:59:13 +0300

    add

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 
 const server = require("http").Server(app);
+// anytime we call a v4 function unique numbers will be generated or every time the app refreshes in this case
+const { v4: uuidV4 } = require("uuid");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.render("room");
+  res.redirect(`/${uuidV4()}`);
+});
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
 });
 
 server.listen(process.env.PORT || 3000);
